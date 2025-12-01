75a6c4b5060b7b87f862d8ae3d9aa77072411e8f
Nitish-hub2 <nithishkomakula2556@gmail.com>
2025-12-01 11:55:05 +0530

    first commit

---
+++ src/main/java/SE/SEExternal23bd1a662w/App.java
@@ -0,0 +1,10 @@
+package SE.SEExternal23bd1a662w;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Maven Java project by 23bd1a662w in external exam 1/12/25");
+    }
+}
---
+++ src/test/java/SE/SEExternal23bd1a662w/AppTest.java
@@ -0,0 +1,19 @@
+package SE.SEExternal23bd1a662w;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
